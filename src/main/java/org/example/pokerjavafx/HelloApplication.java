814b55b90b2212d7bb1f46452899a7e767e99eb9
haaksorl <hakon.sorli@ntnu.no>
2024-03-19 13:45:34 +0100

    La inn bakgrunn og knapper

--- src/main/java/org/example/pokerjavafx/HelloApplication.java
+++ src/main/java/org/example/pokerjavafx/HelloApplication.java
@@ -3,6 +3,10 @@ package org.example.pokerjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +14,30 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Image pokerBackground = new Image(getClass().getResourceAsStream("pokerbilde.jpg"));
+
+        ImageView backgroundImageView = new ImageView(pokerBackground);
+
+        backgroundImageView.setFitHeight(600);
+        backgroundImageView.setFitWidth(1000);
+        backgroundImageView.setPreserveRatio(false);
+
+
+        StackPane root = new StackPane();
+        root.getChildren().add(backgroundImageView);
+
+        Button dealHand = new Button("Deal Hand");
+        Button checkHand = new Button("Check hand");
+        Button checkFlush = new Button("Check flush");
+        Button checkSpadeQueen = new Button("Check spade queen");
+        Button checkSum = new Button("Check sum");
+        Button checkHearts = new Button("Check hearts");
+
+        root.getChildren().add(dealHand);
+        root.getChildren().add(checkHand);
+
+        Scene scene = new Scene(root, 1000, 600);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

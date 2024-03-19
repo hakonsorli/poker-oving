60afb32dd1fdae2067bcadbfc022e24318483b26
haaksorl <hakon.sorli@ntnu.no>
2024-03-19 13:43:01 +0100

    Deckmanager for å bruke metoder i javafx

---
+++ src/main/java/org/example/pokerjavafx/DeckManager.java
@@ -0,0 +1,30 @@
+package org.example.pokerjavafx;
+
+import java.util.List;
+
+public class DeckManager {
+    private PlayingCard p = new PlayingCard(' ', 0);
+    private List<PlayingCard> deck;
+    private List<PlayingCard> hand;
+    char[] suit ={'S', 'H', 'D', 'C'};
+    int[] values={1,2,3,4,5,6,7,8,9,10,11,12,13};
+     public DeckManager(){
+         deck = p.makeDeck(suit, values);
+    }
+    List<PlayingCard> dealHand(){
+         hand = p.dealHand(deck);
+         return hand;
+    }
+    void checkFlush(){
+         p.isFlush(hand);
+    }
+    String checkHearts(){
+         return p.getHearts(hand);
+    }
+    void spadeQueen(){
+         p.containsSpadeQueen(hand);
+    }
+    int getSum(){
+         return p.getValue(hand);
+    }
+}

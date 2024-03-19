45d527b79226b7e11a92d07cdf500519f22e880b
haaksorl <hakon.sorli@ntnu.no>
2024-03-19 22:59:49 +0100

    Implementerte metoder fra DeckManager, tekstfelt til sjekker, VBOX og HBOX for å vise utdelte kort, og sjekkene (flush og div)

--- src/main/java/org/example/pokerjavafx/HelloApplication.java
+++ src/main/java/org/example/pokerjavafx/HelloApplication.java
@@ -6,14 +6,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HelloApplication extends Application {
+    private DeckManager dm = new DeckManager();
     @Override
     public void start(Stage stage) throws IOException {
+
+        VBox vBox = new VBox(15);
+        HBox cardDisplayHBox = new HBox(5);
+        VBox allContent = new VBox(20);
+        cardDisplayHBox.setLayoutY(100);
         Image pokerBackground = new Image(getClass().getResourceAsStream("pokerbilde.jpg"));
 
         ImageView backgroundImageView = new ImageView(pokerBackground);
@@ -27,14 +37,73 @@ public class HelloApplication extends Application {
         root.getChildren().add(backgroundImageView);
 
         Button dealHand = new Button("Deal Hand");
-        Button checkHand = new Button("Check hand");
-        Button checkFlush = new Button("Check flush");
-        Button checkSpadeQueen = new Button("Check spade queen");
-        Button checkSum = new Button("Check sum");
-        Button checkHearts = new Button("Check hearts");
+        Text dealHandResult = new Text();
 
-        root.getChildren().add(dealHand);
-        root.getChildren().add(checkHand);
+        Button checkFlush = new Button("Check flush");
+        Text checkFlushResult = new Text();
+
+        Button checkSpadeQueen = new Button("Check spade queen");
+        Text spadeQueenResult = new Text();
+        Button checkSum = new Button("Check sum");
+        Text checkSumResult = new Text();
+
+        Button checkHearts = new Button("Check hearts");
+        Text checkHeartsResult = new Text();
+
+        checkFlush.setDisable(true);
+        checkSpadeQueen.setDisable(true);
+        checkSum.setDisable(true);
+        checkHearts.setDisable(true);
+
+        dealHand.setOnAction(e -> {
+            List<PlayingCard> hand = dm.dealHand();
+            cardDisplayHBox.getChildren().clear();
+            for (PlayingCard card : hand) {
+                StackPane cardGraphic = dm.cardGraphic(card);
+                cardDisplayHBox.getChildren().add(cardGraphic);
+            }
+
+            dealHandResult.setText("Hand dealt: " + hand.toString());
+            checkFlushResult.setText(" ");
+            spadeQueenResult.setText(" ");
+            checkSumResult.setText(" ");
+            checkHeartsResult.setText(" ");
+            checkFlush.setDisable(false);
+            checkSpadeQueen.setDisable(false);
+            checkSum.setDisable(false);
+            checkHearts.setDisable(false);
+
+        });
+
+
+        checkFlush.setOnAction(e -> {
+            boolean isFlush = dm.checkFlush();
+            checkFlushResult.setText("Is flush: " + isFlush);
+        });
+
+        checkSpadeQueen.setOnAction(e -> {
+            boolean hasSpadeQueen= dm.spadeQueen();
+            spadeQueenResult.setText("Spade queen in hand: " + hasSpadeQueen);
+
+
+        });
+        checkSum.setOnAction(e -> {
+            int sum = dm.getSum();
+            checkSumResult.setText("Sum: " + sum);
+
+
+        });
+        checkHearts.setOnAction(e -> {
+            String heart = dm.checkHearts();
+            checkHeartsResult.setText("Hearts: " + heart);
+
+
+        });
+        vBox.getChildren().addAll(dealHand,dealHandResult,checkFlush, checkFlushResult, checkSpadeQueen, spadeQueenResult,
+                checkHearts, checkHeartsResult, checkSum, checkSumResult);
+
+        allContent.getChildren().addAll(vBox, cardDisplayHBox);
+        root.getChildren().addAll(allContent);
 
         Scene scene = new Scene(root, 1000, 600);
 

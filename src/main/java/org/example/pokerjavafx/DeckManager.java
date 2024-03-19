853c9318b69c08ce8f366e3d2b589f48aa53f9e6
haaksorl <hakon.sorli@ntnu.no>
2024-03-19 22:55:35 +0100

    Lagde metoder i DM for å vise kortene

--- src/main/java/org/example/pokerjavafx/DeckManager.java
+++ src/main/java/org/example/pokerjavafx/DeckManager.java
@@ -1,8 +1,14 @@
 package org.example.pokerjavafx;
 
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
 import java.util.List;
 
 public class DeckManager {
+
     private PlayingCard p = new PlayingCard(' ', 0);
     private List<PlayingCard> deck;
     private List<PlayingCard> hand;
@@ -27,4 +33,29 @@ public class DeckManager {
     int getSum(){
          return p.getValue(hand);
     }
+    public StackPane cardGraphic(PlayingCard card) {StackPane cardPane = new StackPane();
+        Rectangle rectangle = new Rectangle(70, 100);
+        rectangle.setFill(Color.CORNSILK);
+        rectangle.setStroke(Color.DARKGREY);
+
+        String cardValue = cardValueDisplay(card);
+        Text text = new Text(cardValue);
+        text.setFill(getSuitColor(card));
+
+        cardPane.getChildren().addAll(rectangle, text);
+        return cardPane;
+                    }
+    private Color getSuitColor(PlayingCard card){
+         return (card.suit == 'H' || card.suit == 'D') ? Color.RED : Color.BLACK;
+    }
+    private String cardValueDisplay(PlayingCard card){
+        return switch (card.value) {
+            case 1 -> "A" + card.suit;
+            case 11 -> "J" + card.suit;
+            case 12 -> "Q" + card.suit;
+            case 13 -> "K" + card.suit;
+            default -> card.value + "" + card.suit;
+        };
+    }
+
 }
